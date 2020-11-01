2a2844ff87f5a974bb420c248e8e7b67aaf9f3e8
Chris Keathley <c@keathley.io>
2020-11-01 18:58:04 -0500

    Prep for publishing

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
-defmodule Norm.MixProject do
+defmodule Vow.MixProject do
   use Mix.Project
+
+  @version "0.1.0"
 
   def project do
     [
+      name: "Vow",
       app: :vow,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/keathley/vow",
+      docs: docs()
     ]
   end
 
@@ -22,7 +29,30 @@ defmodule Norm.MixProject do
   defp deps do
     [
       {:decorator, "~> 1.3"},
-      {:ex_doc, "~> 0.23"},
+
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
+    ]
+  end
+
+  defp description do
+    """
+    Vow provides a system for Design by Contract in Elixir.
+    """
+  end
+
+  def package do
+    [
+      name: "Vow",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/keathley/vow"}
+    ]
+  end
+
+  def docs do
+    [
+      main: "Vow",
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/keathley/vow"
     ]
   end
 end
